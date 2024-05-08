4f84f522d9e92c56d936fa2efc92589a397b9e95
Wonjin Kim <wjkim9653@gmail.com>
2024-05-08 23:38:04 +0900

    added: json_preprocess.py -> Takes the resulting files after running download.sh (train.de-en.de/en and valid.de-en.de/en), and creates JSON formatted files for train and valid sets. (resulting files: train.json, valid.json)

---
+++ json_preprocess.py
@@ -0,0 +1,47 @@
+"""
+Created on Jan 26, 2018
+download.sh 실행 후, datasets/iwslt17.de.en/ 디렉토리에 생성되는 train.de-en.de, train.de-en.en, valid.de-en.de, valid.de-en.en을 취합하여
+{input:de_sentence, output:en_sentence} 형태로 구성되는 json 파일을 생성하기 위한 파이썬 코드임
+
+Terminal Command: python json_preprocess.py source_language_file_dir target_language_file_dir output_file_dir
+"""
+
+import json
+import sys
+
+
+def create_json(source_language_file, target_language_file, output_json_file):
+    with open(source_language_file, 'r', encoding='utf-8') as f_source, open(target_language_file, 'r', encoding='utf-8') as f_target:
+        source_lines = f_source.readlines()
+        target_lines = f_target.readlines()
+
+    # Check if both files have the same number of lines
+    if len(source_lines) != len(target_lines):
+        print("Error: Files have different number of lines.")
+        return
+
+    data = []  # 딕셔너리{input:source_line, output:target_line}들을 담은 리스트 생성
+    for source_line, target_line in zip(source_lines, target_lines):
+        data.append({'input': source_line.strip(), 'output': target_line.strip()})
+
+    # Write JSON to file
+    with open(output_json_file, 'w', encoding='utf-8') as f_out:
+        json.dump(data, f_out, ensure_ascii=False, indent=2)
+
+    print("JSON file created:", output_json_file)
+
+# Test Runs
+# create_json("datasets/iwslt17.de.en/train.de-en.de", "datasets/iwslt17.de.en/train.de-en.en", "datasets/iwslt17.de.en/train.json")
+# create_json("datasets/iwslt17.de.en/valid.de-en.de", "datasets/iwslt17.de.en/valid.de-en.en", "datasets/iwslt17.de.en/valid.json")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python json_preprocess.py <src_lang_file> <tgt_lang_file> <output_file>")
+        sys.exit(1)
+
+    src_lang_file = sys.argv[1]
+    tgt__lang_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    create_json(src_lang_file, tgt__lang_file, output_file)
