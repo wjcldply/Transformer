7db6039c0bdfc6e69751e5606419631e3f0a0acc
wjcldply <wjkim9653@gmail.com>
2024-05-17 03:52:05 +0900

    added: run_translation.sh -> batch job bash file to run run_translation (training T5-small) on Seraph GPU Cluster

---
+++ run_translation.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#SBATCH -J wjkim_run_translation
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=24G
+#SBATCH -p batch_ce_ugrad
+#SBATCH -t 1-0
+#SBATCH -o logs/run_translation-%A.out
+
+pwd
+which python
+hostname
+
+python run_translation.py --model_name_or_path google-t5/t5-small \
+                          --do_train \
+                          --do_eval \
+                          --source_lang input \
+                          --target_lang output \
+                          --source_prefix "translate German to English: " \
+                          --train_file datasets/iwslt17.de.en/train.jsonl \
+                          --validation_file datasets/iwslt17.de.en/valid.jsonl \
+                          --output_dir /tmp/tst_translation \
+                          --per_device_train_batch_size=4 \
+                          --per_device_eval_batch_size=4 \
+                          --overwrite_output_dir \
+                          --predict_with_generate
+
+exit 0
+
