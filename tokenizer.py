0152f2826867a6475bf76e46e90dadd391528d67
Wonjin Kim <wjkim9653@gmail.com>
2024-05-18 13:28:27 +0900

    updated: T5-Baseline.md, tokenizer.py -> Renamed t5small_baseline.md to T5-Baseline.md -> updated tokenizer.py and added train.spm() function

--- tokenizer.py
+++ tokenizer.py
@@ -7,7 +7,7 @@ spm_path = '{}/sp'.format(datapath)
 
 
 def main():
-    train_txt = ",".join([src_path, dest_path])
+    """train_txt = ",".join([src_path, dest_path])
     vocab_size = 37000
     character_coverage = 1.0
     model_name = spm_path
@@ -17,8 +17,10 @@ def main():
     cmd = f'--input={train_txt} --model_prefix={model_name} --vocab_size={vocab_size} --character_coverage={character_coverage} --model_type={model_types[1]} --user_defined_symbols={user_defined_symbols}'
 
     spm.SentencePieceTrainer.Train(cmd)
-
+    """
+    spm = train_spm()
     sp = spm.SentencePieceProcessor(model_file=spm_path+'.model')
+
     en_line = "How much suffering there is to get through!"
     # en_line = "Whatever that doesn't kill you makes you stronger."
     de_line = "Wie viel ist aufzuleiden!"
@@ -32,6 +34,24 @@ def main():
     print(sp.bos_id())
     print(sp.eos_id())
     print(sp.unk_id())
+
+
+
+
+def train_spm():
+    train_txt = ",".join([src_path, dest_path])
+    vocab_size = 37000
+    character_coverage = 1.0
+    model_name = spm_path
+    model_types = ['unigram', 'bpe', 'char', 'word']
+    user_defined_symbols = "[PAD],[UNK],[CLS],[SEP],[MASK],[BOS],[EOS]"
+
+    cmd = f'--input={train_txt} --model_prefix={model_name} --vocab_size={vocab_size} --character_coverage={character_coverage} --model_type={model_types[1]} --user_defined_symbols={user_defined_symbols}'
+
+    spm.SentencePieceTrainer.Train(cmd)
+
+    return spm
+
 
 if __name__ == '__main__':
     main()
